1067e1a0d4c186dd9ecd250151eda55140251323
Christine Stone <cc@battab.com>
2017-07-11 23:29:15 -0400

    add responsive

--- resources/views/display/nendoroidbox.blade.php
+++ resources/views/display/nendoroidbox.blade.php
@@ -18,12 +18,12 @@
             </tr>
             <tr>
                 <td>#690 Yamanbagiri Kunihiro</td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/5.JPG"></td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/7.JPG"></td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/6.JPG"></td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/8.JPG"></td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/9.JPG"></td>
-                <td><img src="/images/review/nendoroid-yamanbagiri-kunihiro/10.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/5.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/7.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/6.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/8.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/9.JPG"></td>
+                <td><img class="img-responsive" src="/images/review/nendoroid-yamanbagiri-kunihiro/10.JPG"></td>
             </tr>
         </table>
 
