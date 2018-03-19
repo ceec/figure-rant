7920e324799fa4e865613d386a1da47a4c52eeba
Christine Stone <cc@battab.com>
2018-03-18 23:55:54 -0600

    add loop to nendo tool

--- app/Http/Controllers/FiguredbController.php
+++ app/Http/Controllers/FiguredbController.php
@@ -21,27 +21,40 @@ class FiguredbController extends Controller
 
     public static function addNendos() {
 
-        //get last inserted ID
-        $last_figure = FigureDB::orderBy('id', 'desc')->first();
+        //do this 900 times
 
-        $next_id = $last_figure->id + 1;
+        for ($i = 13; $i<=900; $i++) {
+            //get last inserted ID
+            $last_figure = FigureDB::orderBy('id', 'desc')->first();
 
-        $f = new FigureDB();
-        $f->name = 'Nendoroid '.$next_id;
-        $f->released = 1;
-        $f->scale_id = 0;
-        $f->manufacturer_id = 1;
-        $f->productline_id = 1;
-        $f->item_number = $next_id;
-        $f->group_id = 0;
-        $f->character_id = 0;
-        $f->sculptor_id = 0;
-        $f->announce_date = '0000-00-00';
-        $f->available_date = '0000-00-00';
-        $f->release_date = '0000-00-00';
-        $f->price = 0;
-        $f->updated_by = 1;
-        $f->save();
+            $next_id = $last_figure->id + 1;
+
+            $f = new FigureDB();
+            $f->name = 'Nendoroid '.$next_id;
+            $f->extended = '';
+            $f->released = 1;
+            $f->scale_id = 0;
+            $f->manufacturer_id = 1;
+            $f->productline_id = 1;
+            $f->item_number = $next_id;
+            $f->group_id = 0;
+            $f->character_id = 0;
+            $f->sculptor_id = 0;
+            $f->announce_date = "2018-01-01";
+            $f->available_date = "2018-01-01";
+            $f->release_date = "2018-01-01";
+            $f->price = 0;
+            $f->height = '';
+            $f->url = '';
+            $f->amazon = '';
+            $f->description = '';
+            $f->amiami_id = '';
+            $f->mandarake_id = '';
+            $f->updated_by = 1;
+            $f->save();
+        }
+
+
 
     }  
 
