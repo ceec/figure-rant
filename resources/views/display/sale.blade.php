a8f8491103c021ef53e84a55d75ecb6067a46355
Christine Stone <cc@battab.com>
2019-02-20 23:20:40 -0700

    update for 2019 sale

--- resources/views/display/sale.blade.php
+++ resources/views/display/sale.blade.php
@@ -32,28 +32,14 @@ For Sale | Figure Rant
             </tr>
             <tr>
               <td>1/8 Uzuki Shimamura by GSC</td>
-              <td>$75</td>
+              <td>$50</td>
               <td>Used</td>
               <td><a href="/review/uzuki-shimamura">Review</a></td>
               <td><a href="https://myfigurecollection.net/item/166431">MFC</a></td>              
-            </tr>
-            <tr>
-              <td>1/8 Rorona by Max Factory</td>
-              <td>$20</td>
-              <td>Used</td>
-              <td><a href="/review/rorona">Review</a></td>
-              <td><a href="https://myfigurecollection.net/item/98904">MFC</a></td>              
             </tr> 
             <tr>
-              <td>1/8 Narika by Alter</td>
-              <td>$20</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/6423">MFC</a></td>              
-            </tr>  
-            <tr>
               <td>Blizzard Tracer - 1st Edition</td>
-              <td>$125</td>
+              <td>$75</td>
               <td>Used</td>
               <td></td>
               <td></td>              
@@ -72,25 +58,18 @@ For Sale | Figure Rant
               <th>MFC Link</th>
             </tr>
             <tr>
-              <td>763 - Li Syaoran</td>
-              <td>$45</td>
-              <td>New</td>
-              <td>GSC Bonus - background</td>
-              <td><a href="https://myfigurecollection.net/item/464586">MFC</a></td>
-            </tr>
-            <tr>
-              <td>720 - Mami Tomoe - Maiko Version</td>
-              <td>$45</td>
+              <td>935 - Kaimina - with GSC Bonus</td>
+              <td>$60</td>
               <td>New</td>
               <td></td>
-              <td><a href="https://myfigurecollection.net/item/464662">MFC</a></td>              
+              <td><a href="">MFC</a></td>              
             </tr>    
             <tr>
-              <td>747 - Mei</td>
+              <td>970-DX - Merlin - with GSC Bonus</td>
               <td>$45</td>
               <td>New</td>
               <td></td>
-              <td><a href="https://myfigurecollection.net/item/549026">MFC</a></td>              
+              <td><a href="">MFC</a></td>              
             </tr>                                                         
           </table>          
 
@@ -114,71 +93,35 @@ For Sale | Figure Rant
               <th>Condition</th>
               <th>Notes</th>
               <th>MFC Link</th>
-            </tr>    
-            <tr>
-              <td>261 - Hatsune Miku - Yukata Version</td>
-              <td>$20</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/117506">MFC</a></td>              
-            </tr> 
-            <tr>
-              <td>570 - Hatsune Miku - Snow Owl Version</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/303155">MFC</a></td>              
-            </tr>              
-            <tr>
-              <td>493 - Hatsune Miku - Snow Bell Version</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/211405">MFC</a></td>              
-            </tr>   
-            <tr>
-              <td>425 - Red</td>
-              <td>$35</td>
-              <td>Used</td>
-              <td>Pokemon Center Version</td>
-              <td><a href="https://myfigurecollection.net/item/215491">MFC</a></td>              
-            </tr>                         
-            <tr>
-              <td>507 - Cynthia</td>
-              <td>$35</td>
-              <td>Used</td>
-              <td>Pokemon Center Version</td>
-              <td><a href="https://myfigurecollection.net/item/291171">MFC</a></td>              
-            </tr> 
-            <tr>
-              <td>537 - N</td>
-              <td>$35</td>
-              <td>Used</td>
-              <td>Pokemon Center Version</td>
-              <td><a href="https://myfigurecollection.net/item/327207">MFC</a></td>              
-            </tr>  
-            <tr>
-              <td>550 - Anna</td>
-              <td>$25</td>
-              <td>Used</td>
-              <td>First release. Olaf's arm broke off, it has been glued back together</td>
-              <td><a href="https://myfigurecollection.net/item/331511">MFC</a></td>              
-            </tr>                        
-
+            </tr>                                                             
             <tr>
               <td>574 - Moritomo Nozomi</td>
-              <td>$40</td>
+              <td>$30</td>
               <td>Used</td>
               <td></td>
               <td><a href="https://myfigurecollection.net/item/287709">MFC</a></td>              
             </tr>               
             <tr>
-              <td>Full Box of Nendoroid Dress up: Cheerleaders - 6 total</td>
+              <td>539 - Harvest Moon Miku</td>
               <td>$40</td>
               <td>Used</td>
               <td></td>
-              <td><a href="https://myfigurecollection.net/item/253370">MFC</a></td>              
-            </tr>                                                                                                                                                                                                                                                                           
+              <td><a href="">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>768 - Harvest Moon Rin</td>
+              <td>$40</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>769 - Harvest Moon Len</td>
+              <td>$40</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="">MFC</a></td>              
+            </tr>                                                                                                                                                                                                                                                                                                   
           </table>   
 
            <img src="/images/sale/0202181421.jpg">
@@ -192,19 +135,13 @@ For Sale | Figure Rant
               <th>MFC Link</th>
             </tr>
             <tr>
-              <td>Shiranui Model Kit</td>
+              <td>Ace Prize Figure</td>
               <td>$20</td>
               <td>New</td>
               <td></td>
-              <td><a href="http://en.kotobukiya.co.jp/product/product-0000001502/">Kotobukiya</a></td>
+              <td><a href="">MFC</a></td>
             </tr>
-            <tr>
-              <td>Cu-Poche Haruka Amami</td>
-              <td>$25</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/117972">MFC</a></td>              
-            </tr>                                                           
+                                                         
           </table> 
         </div>
 
