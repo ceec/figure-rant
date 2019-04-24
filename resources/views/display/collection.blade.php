d919c47e198bc19d9bc89b9f60386ee668e0ec63
Christine Stone <cc@battab.com>
2019-04-23 22:03:00 -0600

    add images to collection

--- resources/views/display/collection.blade.php
+++ resources/views/display/collection.blade.php
@@ -10,12 +10,26 @@ About - figurerant.com
     <div class="row">
         <div class="col-md-12">
             <h2>Collection</h2>
+            <div class="row">
+            <?php $x = 0; ?>
 
             @foreach($figures as $figure)
-                {{$figure->figureDB->name}}<br>
+                <div class="col-md-3">
+                    @if($figure->figureDB->productline_id == 7)
+                        <img class="img-responsive" src="/images/nendoroids/{{$figure->figureDB->url}}-released.jpg">
+                    @else
+                        <img class="img-responsive" src="/images/scales/{{$figure->figureDB->url}}-released.jpg">
+                    @endif                    
+                    {{$figure->figureDB->name}}<br>
+                </div>
+                <?php $x++; ?>
+                @if ($x%4 == 0)
+                    </div>
+                    <div class="row">
+                @endif
             @endforeach
                                
-                             
+            </div>          
         </div>
     </div>
 </div>
