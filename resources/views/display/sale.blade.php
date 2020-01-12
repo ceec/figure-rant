9edfad5f1a176772c04d1be8ff8b07399465d560
Christine Stone <cc@battab.com>
2020-01-12 13:44:41 -0700

    Update sale page

--- resources/views/display/sale.blade.php
+++ resources/views/display/sale.blade.php
@@ -12,6 +12,154 @@ For Sale | Figure Rant
 
           <h1>For Sale</h1>
           <p>Shipping from Colorado, US. Will ship worldwide, shipping not included.</p>
+          
+          <h2>Nendoroids</h2>         
+
+          <h3>Opened</h3>
+          <div class="row">
+            <div class="col-md-12">
+              <img class="img-responsive" src="/images/sale/2020-01-12-nendoroids.jpg">
+            </div>
+          
+          <table class="table">
+            <tr>
+              <th>Name</th>
+              <th>Price (USD)</th>
+              <th>Condition</th>
+              <th>Notes</th>
+              <th>MFC Link</th>
+            </tr>                                                             
+            <tr>
+              <td>359 - Drossel (Charming)</td>
+              <td>$40</td>
+              <td>Used</td>
+              <td>Light up eyes still work great</td>
+              <td><a href="https://myfigurecollection.net/item/169484">MFC</a></td>              
+            </tr>               
+            <tr>
+              <td>368 - Hachikuji Mayoi</td>
+              <td>$40</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/166913">MFC</a></td>              
+            </tr>  
+            <tr>
+              <td>752 - Serval</td>
+              <td>$25</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/549358">MFC</a></td>              
+            </tr>  
+            <tr>
+              <td>Lucky Beast</td>
+              <td>$15</td>
+              <td>Used</td>
+              <td>Not a nendo but will sell for $5 if bought with Serval</td>
+              <td><a href="https://myfigurecollection.net/item/604210">MFC</a></td>              
+            </tr>              
+            <tr>
+              <td>936 - Saber/Miyamoto Musashi</td>
+              <td>$35</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/675998">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>Nendoroid Co-de Toudou Shion - Baby Monster Cyalume</td>
+              <td>$5</td>
+              <td>Used</td>
+              <td>She suffered a fall which left scraping on her head as well damage to her hair piece.</td>
+              <td><a href="https://myfigurecollection.net/item/396844">MFC</a></td>              
+            </tr>                                   
+            <tr>
+              <td>400 - Kinomoto Sakura</td>
+              <td>$60</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/136458">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>490 - Daidouji Tomoyo</td>
+              <td>$60</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/218618">MFC</a></td>              
+            </tr> 
+            <tr>
+              <td>763 - Li Syaoran</td>
+              <td>$60</td>
+              <td>Used</td>
+              <td>Has unopened GSC bonus</td>
+              <td><a href="https://myfigurecollection.net/item/464586">MFC</a></td>              
+            </tr>  
+            <tr>
+              <td>Nendoroid Co-de Kinomoto Sakura - Black Cat Maid</td>
+              <td>$50</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/396844">MFC</a></td>              
+            </tr> 
+            <tr>
+              <td>Nendoroid Co-de Kinomoto Sakura - Angel Crown</td>
+              <td>$40</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/444485">MFC</a></td>              
+            </tr>                                                             
+            <tr>
+              <td>481 - Kumano</td>
+              <td>$15</td>
+              <td>Used</td>
+              <td>One of her tiny guns on her arms is missing. There are extras on all the other arms to use when displaying.</td>
+              <td><a href="https://myfigurecollection.net/item/236164">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>482 - Suzuya</td>
+              <td>$25</td>
+              <td>Used</td>
+              <td>GSC bonus of sleeve and extra base</td>
+              <td><a href="https://myfigurecollection.net/item/236169">MFC</a></td>              
+            </tr> 
+            <tr>
+              <td>551 - Oyodo</td>
+              <td>$20</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/287704">MFC</a></td>              
+            </tr>                                                                                                                                                                                                                                                                                                                              
+            <tr>
+              <td>490 - Tsurumaru Kuninaga</td>
+              <td>$25</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/322188">MFC</a></td>              
+            </tr>             
+            <tr>
+              <td>651 - Monoyoshi Sadamune</td>
+              <td>$35</td>
+              <td>Used</td>
+              <td>GSC bonus keychain</td>
+              <td><a href="https://myfigurecollection.net/item/396883">MFC</a></td>              
+            </tr>   
+            <tr>
+              <td>677 - Okurikara</td>
+              <td>$35</td>
+              <td>Used</td>
+              <td>GSC bonus keychain</td>
+              <td><a href="https://myfigurecollection.net/item/391850">MFC</a></td>              
+            </tr>                           
+            <tr>
+              <td>NNendoroid More: Konnosuke Face Parts Case</td>
+              <td>$15</td>
+              <td>Used</td>
+              <td></td>
+              <td><a href="https://myfigurecollection.net/item/463554">MFC</a></td>              
+            </tr>
+
+             
+          </table>   
+
+
           <h2>Scales</h2>
           <div class="row">
             <div class="col-md-6">
@@ -32,98 +180,22 @@ For Sale | Figure Rant
               <th>MFC Link</th>
             </tr>
             <tr>
-              <td>1/8 Uzuki Shimamura by GSC</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td><a href="/review/uzuki-shimamura">Review</a></td>
-              <td><a href="https://myfigurecollection.net/item/166431">MFC</a></td>              
+              <td>1/8 Allen Walker by Aniplex</td>
+              <td>$200 with free US shipping</td>
+              <td>Box damaged but figure unopened</td>
+              <td>His box suffered shipping damage :(. The left back corner was smushed in but the plastic shell took all the damage.</td>
+              <td><a href="https://myfigurecollection.net/item/460687">MFC</a></td>              
             </tr> 
-          <!--  <tr>
+          <tr>
               <td>Blizzard Tracer - 1st Edition</td>
               <td>$75</td>
               <td>Used</td>
-              <td></td>
+              <td>She has the first face produced for this figure.</td>
               <td></td>              
-            </tr>  -->                                                                     
+            </tr>                                                                   
           </table>
-          <h2>Nendoroids</h2>
-          <h3>New - Unopened box, bought from GSC</h3>
-          <img src="/images/sale/0305191616a.jpg">
-          
 
-          
-          <table class="table">
-            <tr>
-              <th>Name</th>
-              <th>Price (USD)</th>
-              <th>Condition</th>
-              <th>Notes</th>
-              <th>MFC Link</th>
-            </tr>
-            <tr>
-              <td>935 - Kaimina</td>
-              <td>$50</td>
-              <td>New</td>
-              <td>with GSC Bonus</td>
-              <td><a href="https://myfigurecollection.net/item/651234">MFC</a></td>              
-            </tr>    
-            <tr>
-              <td>970-DX - Merlin Magus of Flowers Version</td>
-              <td>$55</td>
-              <td>New</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/741846">MFC</a></td>              
-            </tr>                                                         
-          </table>          
-
-          <h3>Opened</h3>
-          <div class="row">
-            <div class="col-md-4">
-              <img class="img-responsive" src="/images/sale/0305191617.jpg">
-            </div>
-            <div class="col-md-8">
-              <img class="img-responsive" src="/images/sale/0305191618.jpg">
-            </div>
-          
-          <table class="table">
-            <tr>
-              <th>Name</th>
-              <th>Price (USD)</th>
-              <th>Condition</th>
-              <th>Notes</th>
-              <th>MFC Link</th>
-            </tr>                                                             
-            <tr>
-              <td>574 - Moritomo Nozomi</td>
-              <td>$30</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/287709">MFC</a></td>              
-            </tr>               
-            <tr>
-              <td>539 - Harvest Moon Miku</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/323689">MFC</a></td>              
-            </tr>   
-            <tr>
-              <td>768 - Harvest Moon Rin</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/549377">MFC</a></td>              
-            </tr>   
-            <tr>
-              <td>769 - Harvest Moon Len</td>
-              <td>$50</td>
-              <td>Used</td>
-              <td></td>
-              <td><a href="https://myfigurecollection.net/item/549380">MFC</a></td>              
-            </tr>                                                                                                                                                                                                                                                                                                   
-          </table>   
-
-          <h2>Others</h2>
+       <!--   <h2>Others</h2>
           <div class="row">
             <div class="col-md-6">
               <img class="img-responsive" src="/images/sale/0305191616_HDR.jpg">
@@ -138,14 +210,14 @@ For Sale | Figure Rant
               <th>MFC Link</th>
             </tr>
             <tr>
-              <td>Ace Prize Figure</td>
+              <td>figma - </td>
               <td>$20</td>
               <td>New</td>
               <td></td>
               <td><a href="https://myfigurecollection.net/item/740376">MFC</a></td>
             </tr>
                                                          
-          </table> 
+          </table> -->
         </div>
 
     </div>
